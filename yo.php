222a816d10d23b97b42296e990645fdd7db91d1c
dificompu <dianafigueroa@gmail.com>
2021-12-09 09:05:22 -0300

    ejercicio unidad 2 php Codo a codo 2021

---
+++ yo.php
@@ -0,0 +1,9 @@
+<?php
+echo '<h1>Actividad unidad 2 </h1>';
+echo('<b>Nombre </b>: Diana<br>');
+echo('<b>Apellido</b>: Figueroa<br>');
+echo('<b>Edad </b>: 59<br>');
+echo('<b>Hobbie: Fotografía<br>');
+echo('<b>Editor de código</b>: Sublime / VSCode<br>');
+echo('<b>Sistema Operativo</b>: Windows<br>');
+?>
